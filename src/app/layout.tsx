0967c3e258dc3c4d87426564dfb7d0a3c0dcaba9
ABDhaikal <kakikuatku@gmail.com>
2025-06-16 14:52:02 +0700

    fix: remove Toaster component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
             </NextAuthProvider>
           </ReactQueryProvider>
         </NuqsProvider>
-        <Toaster position="top-right" />
       </body>
     </html>
   );
