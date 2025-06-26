49700dec450d36650dd53ef2f9e492739b17102e
Hibban Faruq W <hibbanhfw@gmail.com>
2025-06-26 15:05:29 +0700

    Fixing bug (#12)
    * fix: route and prescription
    
    * fix: route and prescription

--- src/hooks/api/auth/useVerifyAccount.ts
+++ src/hooks/api/auth/useVerifyAccount.ts
@@ -1,13 +1,23 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axios";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
 
 const useVerifyAccount = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (token: string) => {
       const { data } = await axiosInstance.post(`/auth/verify/${token}`);
       return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["get-account"] });
+      toast.success("Account verified successfully");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data.message);
     },
   });
 };
